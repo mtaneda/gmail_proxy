040be957780b78aea2084870c14cafa9f9ed1339
mtaneda <mtaneda@misp.jp>
2016-06-30 12:23:41 +0900

    refactor: transform tab to 4 spaces.

--- gmail_proxy.py
+++ gmail_proxy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 ; tab-width: 8 -*-
 """ gmail proxy
 
 .forward に書いておくと、gmailに転送してくれるプロクシコマンド
@@ -74,20 +74,20 @@ class StreamUtil:
         Return value:
         更新されたoffline_flag
         """
-	if not offline_flag:
-		s.send(data)
-		logging.debug('SEND: ' + data.strip())
+        if not offline_flag:
+            s.send(data)
+            logging.debug('SEND: ' + data.strip())
 
-		buf = s.recv(BUFSIZ)
-		if not buf:
-                        logging.error('Receive error')
-			offline_flag = True
-		else:
-			logging.debug('RECV: ' + buf.strip())
-                        stcode = buf.split(' ')[0]
-			if stcode != okstcode:
-				logging.error('Status error: ' + stcode + ' required was ' + okstcode)
-				offline_flag = True
+            buf = s.recv(BUFSIZ)
+            if not buf:
+                logging.error('Receive error')
+                offline_flag = True
+            else:
+                logging.debug('RECV: ' + buf.strip())
+                stcode = buf.split(' ')[0]
+                if stcode != okstcode:
+                    logging.error('Status error: ' + stcode + ' required was ' + okstcode)
+                    offline_flag = True
 
         return offline_flag
 
@@ -102,10 +102,10 @@ class Gmail:
     ステータス列挙体
     """
     state = (
-	STATE_FIRST_LINE,	# From 行
-	STATE_HEADER,		# ヘッダ
-	STATE_BODY,		# ヘッダが終わった後の行
-        ) = range(0, 3)
+        STATE_FIRST_LINE,       # From 行
+        STATE_HEADER,           # ヘッダ
+        STATE_BODY,             # ヘッダが終わった後の行
+    ) = range(0, 3)
 
     def __init__(self):
         """ __init__
@@ -115,78 +115,78 @@ class Gmail:
     def do_proxy(self):
         """ 本体
         """
-	mail_from = ''
+        mail_from = ''
         su = StreamUtil()
 
-	s = None
-	for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
-		af, socktype, proto, canonname, sa = res
-	        try:
-			s = socket.socket(af, socktype, proto)
-		except socket.error, msg:
-			s = None
-			continue
-		try:
-			s.connect(sa)
-		except socket.error, msg:
-			s.close()
-			s = None
-			continue
-		break
+        s = None
+        for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+            af, socktype, proto, canonname, sa = res
+            try:
+                s = socket.socket(af, socktype, proto)
+            except socket.error, msg:
+                s = None
+                continue
+            try:
+                s.connect(sa)
+            except socket.error, msg:
+                s.close()
+                s = None
+                continue
+            break
 
-	if s is None:
-		logging.error('Can\'t connect to: ' + HOST)
-	else:
-            	logging.debug('Connected to: ' + HOST)
-		offline = False
+        if s is None:
+            logging.error('Can\'t connect to: ' + HOST)
+        else:
+            logging.debug('Connected to: ' + HOST)
+            offline = False
 
         # TODO メソッド抽出
-	if not offline:
-		buf = s.recv(BUFSIZ)
-		if not buf:
-                        logging.error('Receive error')
-			offline = True
-		else:
-			logging.debug('RECV: ' + buf.strip())
-                        stcode = buf.split(' ')[0]
-			if stcode != '220':
-				logging.error('Status error: ' + stcode + ' required was ' + okstcode)
-				offline = True
+        if not offline:
+            buf = s.recv(BUFSIZ)
+            if not buf:
+                logging.error('Receive error')
+                offline = True
+            else:
+                logging.debug('RECV: ' + buf.strip())
+                stcode = buf.split(' ')[0]
+                if stcode != '220':
+                    logging.error('Status error: ' + stcode + ' required was ' + okstcode)
+                    offline = True
         # TODO メソッド抽出
-	if not offline:
-            	ehlomsg = 'EHLO ' + MYDOMAIN + '\r\n'
-                s.send(ehlomsg)
-                logging.debug('SEND: ' + ehlomsg.strip())
+        if not offline:
+            ehlomsg = 'EHLO ' + MYDOMAIN + '\r\n'
+            s.send(ehlomsg)
+            logging.debug('SEND: ' + ehlomsg.strip())
+            buf = su.readline(s)
+            logging.debug('RECV: ' + buf.strip())
+            # TODO エラー処理強化
+            while buf.split(' ')[0] != '250':
                 buf = su.readline(s)
                 logging.debug('RECV: ' + buf.strip())
-                # TODO エラー処理強化
-                while buf.split(' ')[0] != '250':
-                        buf = su.readline(s)
-                        logging.debug('RECV: ' + buf.strip())
 
-	state = self.STATE_FIRST_LINE
-	for line in iter(sys.stdin.readline, ""):
-                # procmail のため標準出力に出す
-            	sys.stdout.write(line)
+        state = self.STATE_FIRST_LINE
+        for line in iter(sys.stdin.readline, ""):
+            # procmail のため標準出力に出す
+            sys.stdout.write(line)
 
-		if state == self.STATE_FIRST_LINE:
-			mail_from = line.split(' ')[1]
-			state = self.STATE_HEADER
-                        offline = su.send_and_recv(s, offline, 'MAIL FROM:<' + mail_from + '>\r\n', '250')
-                        offline = su.send_and_recv(s, offline, 'RCPT TO:<' + MYADDR + '>\r\n', '250')
-                        offline = su.send_and_recv(s, offline, 'DATA\r\n', '354')
+            if state == self.STATE_FIRST_LINE:
+                mail_from = line.split(' ')[1]
+                state = self.STATE_HEADER
+                offline = su.send_and_recv(s, offline, 'MAIL FROM:<' + mail_from + '>\r\n', '250')
+                offline = su.send_and_recv(s, offline, 'RCPT TO:<' + MYADDR + '>\r\n', '250')
+                offline = su.send_and_recv(s, offline, 'DATA\r\n', '354')
 
-		if state == self.STATE_HEADER:
-			if not offline:
-				s.send(line)
-				logging.debug('SEND: ' + line.strip())
-			if line == '\n':
-				state = self.STATE_BODY
+            if state == self.STATE_HEADER:
+                if not offline:
+                    s.send(line)
+                    logging.debug('SEND: ' + line.strip())
+                if line == '\n':
+                    state = self.STATE_BODY
 
-		if state == self.STATE_BODY:
-			if not offline:
-				s.send(line)
-				logging.debug('SEND: ' + line.strip())
+            if state == self.STATE_BODY:
+                if not offline:
+                    s.send(line)
+                    logging.debug('SEND: ' + line.strip())
 
         offline = su.send_and_recv(s, offline, '\r\n.\r\n', '250')
         offline = su.send_and_recv(s, offline, 'QUIT\r\n', '221')
